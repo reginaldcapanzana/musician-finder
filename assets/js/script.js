1965c75d6a84891f4b8aeb704677d90f32d43cbc
Gabriel Guerin <gjguerin96@gmail.com>
2021-10-23 13:49:15 -0700

    added tour date info from bandsintown

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,12 @@
 var button = document.querySelector('#btn')
 var bandName = document.querySelector('#bandName');
-//var userUrl = document.createElement('p');
 var bandPic = document.querySelector('#bandPic')
+var listContainer = document.querySelector("#listContainer")
 
 button.addEventListener("click", function() {
+    // bandName.textContent = ""
+    // bandPic.setAttribute('src', "")
+    listContainer.innerHTML = ""
     var searchVal = document.querySelector('#search').value;
     // console.log(searchVal)
     var requestUrl = "https://rest.bandsintown.com/artists/" + searchVal + "?app_id=3cc6769e7ff614003f89926a784e3cd0"
@@ -16,4 +19,18 @@ button.addEventListener("click", function() {
             bandName.textContent = data.name;
             bandPic.setAttribute('src', data.image_url)
         });
+        var eventsUrl = "https://rest.bandsintown.com/artists/" + searchVal + "/events?app_id=3cc6769e7ff614003f89926a784e3cd0&date=upcoming"
+    fetch(eventsUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            for (var i = 0; i < data.length; i++) {
+                var tourInfo = document.createElement('li');
+                var tourDate = data[i].datetime.slice(0, 10)
+                tourInfo.textContent = data[i].venue.location + " at " + data[i].venue.name + ". Date: " + tourDate
+                listContainer.append(tourInfo)
+            }
+        })
     })
