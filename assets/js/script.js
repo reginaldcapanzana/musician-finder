31d2c2f5875e4efe306bfdbe727de1d43082ad94
Reginald Capanzana <reggiecapanzana@gmail.com>
2021-10-23 21:14:14 -0700

    Initial changes for Spotify API

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,19 @@ var bandName = document.querySelector('#bandName');
 //var userUrl = document.createElement('p');
 var bandPic = document.querySelector('#bandPic')
 
+//Variables needed for Spotify API
+const clientID = 'f0956f3b8a6d41f28174c3145226555f';
+const clientSecret = 'f437039b0ecf433d94808485272815a8';
+const redirectURI = 'https://reginaldcapanzana.github.io/musician-finder';
+
+//Spotify API Token
+// Will run on start up. Need Token before accessing anything from the SpotifyAPI
+const spotifyAPIController = (function(){
+    const getToken = async () => {
+        const result = await fetch('https://accounts.spotify.com')
+    }
+})();
+
 button.addEventListener("click", function() {
     var searchVal = document.querySelector('#search').value;
     // console.log(searchVal)
@@ -16,4 +29,15 @@ button.addEventListener("click", function() {
             bandName.textContent = data.name;
             bandPic.setAttribute('src', data.image_url)
         });
+
+    //Top Songs Spotify API
+    var topSongsURL = "https://api.spotify.com/v1/artists/" + searchVal + "/top-tracks";
+    fetch(topSongsURL)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(data){
+            console.log(data);
+        })
+
     })
