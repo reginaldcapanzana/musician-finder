a8914fac02a4a6a60aefd375ea8816e70d8544d2
Gabriel Guerin <gjguerin96@gmail.com>
2021-10-25 21:13:58 -0700

    added ticket links and error messages for bandsintown api

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,33 +4,55 @@ var bandPic = document.querySelector('#bandPic')
 var listContainer = document.querySelector("#listContainer")
 
 button.addEventListener("click", function() {
-    // bandName.textContent = ""
-    // bandPic.setAttribute('src', "")
-    listContainer.innerHTML = ""
-    var searchVal = document.querySelector('#search').value;
-    // console.log(searchVal)
+    bandName.textContent = ""
+    bandPic.setAttribute('src', "")
+    upcomingShows.innerHTML = ""
+    var searchVal = document.querySelector('#search').value
     var requestUrl = "https://rest.bandsintown.com/artists/" + searchVal + "?app_id=3cc6769e7ff614003f89926a784e3cd0"
+    
     fetch(requestUrl)
         .then(function (response) {
-            return response.json();
+            return response.json()
         })
         .then(function (data) {
-            console.log(data);
-            bandName.textContent = data.name;
-            bandPic.setAttribute('src', data.image_url)
-        });
-        var eventsUrl = "https://rest.bandsintown.com/artists/" + searchVal + "/events?app_id=3cc6769e7ff614003f89926a784e3cd0&date=upcoming"
-    fetch(eventsUrl)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data);
-            for (var i = 0; i < data.length; i++) {
-                var tourInfo = document.createElement('li');
-                var tourDate = data[i].datetime.slice(0, 10)
-                tourInfo.textContent = data[i].venue.location + " at " + data[i].venue.name + ". Date: " + tourDate
-                listContainer.append(tourInfo)
+            // console.log(data)
+            if (Object.keys(data).length == 0) {
+                bandName.textContent = "Artist not found"
+                bandPic.setAttribute('src', "")
+                return 
+            }
+            else {
+                bandName.textContent = data.name
+                bandPic.setAttribute('src', data.image_url)
+                
+                var eventsUrl = "https://rest.bandsintown.com/artists/" + searchVal + "/events?app_id=3cc6769e7ff614003f89926a784e3cd0&date=upcoming"
+                fetch(eventsUrl)
+                .then(function (response) {
+                    return response.json()
+                })
+                .then(function (data) {
+                // console.log(data)
+                if (Object.keys(data).length == 0) {
+                        var noTourInfo = document.createElement('p')
+                        noTourInfo.textContent = 'No upcoming shows!'
+                        upcomingShows.append(noTourInfo)
+                        return
+                    }
+                else {
+                    for (var i = 0; i < data.length; i++) {
+                        var tourInfo = document.createElement('p')
+                        var tourDate = data[i].datetime.slice(0, 10)
+                        tourInfo.textContent = data[i].venue.location + " at " + data[i].venue.name + ". Date: " + tourDate + " "
+                        
+                        var ticketPlaceholder = document.createElement('a')
+                        var linkText = document.createTextNode("Get tickets!")
+                        ticketPlaceholder.appendChild(linkText)
+                        ticketPlaceholder.href = data[i].url
+                        
+                        upcomingShows.append(tourInfo)
+                        tourInfo.append(ticketPlaceholder)
+                    }}
+                })
             }
         })
-    })
+})
